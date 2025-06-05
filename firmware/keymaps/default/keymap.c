39694c1ba7d58bd5c62aca1370f3fa3cf5a906d3
Monsieur Skibidi <204919318+heckerguy@users.noreply.github.com>
2025-06-05 19:44:27 -0400

    keymap is now the actual one

--- firmware/keymaps/default/keymap.c
+++ firmware/keymaps/default/keymap.c
@@ -5,7 +5,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_A,    KC_I,   KC_S,   KC_H,
+        KC_DOT,    KC_MINS,   KC_SPC,   KC_H,
         KC_LCTL,   KC_RGUI
     )
 };
